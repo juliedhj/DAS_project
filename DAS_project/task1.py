e0494dc85305d8b7c9b48848346f7e09d6277724
Julie <juliedhj@gmail.com>
2022-05-12 17:27:46 +0200

    uncommented

--- DAS_project/task1.py
+++ DAS_project/task1.py
@@ -10,12 +10,12 @@ data = mnist.load_data()
 (X_train, y_train), (X_test, y_test) = data
 
 #Reshape the dataset to access every pixel
-#X_train = X_train.reshape((X_train.shape[0], 28*28)).astype('float32')
-#X_test = X_test.reshape((X_test.shape[0], 28*28)).astype('float32')
+X_train = X_train.reshape((X_train.shape[0], 28*28)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], 28*28)).astype('float32')
 
 #Get value of every pixel as a number between 0 and 1
-#X_train = X_train / 255
-#X_test = X_test / 255
+X_train = X_train / 255
+X_test = X_test / 255
 
 #1
 # Digit = 4
@@ -34,7 +34,7 @@ X_train = np.array(X_train)[p.astype(int)]
 def splitSet(n):
     subsets_X = np.array_split(X_train, n)
     subsets_y = np.array_split(y_train, n)
-    #print(subsets_X, subsets_y)
+    print(subsets_X, subsets_y)
 
 splitSet(5)
 
